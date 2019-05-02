6eeff3203802d0931a07ef4195e7e8db72f1cd97
Katherine Hansen <13894347+kahansen2000@users.noreply.github.com>
2019-05-02 02:38:08 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+*Katherine Hansen
+*2326665
+*kahansen@chapman.edu
+*CPSC350-01
+*Assignment 5: Database
+*/
+
+#include <iostream>
+#include "Database.h"
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	Database d;
+	d.printMenu();
+	
+	return 0;
+}
